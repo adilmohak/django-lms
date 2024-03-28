f1f00ede9bd53b7a62a1e5f6f44ba98571ce76c7
tertese <tertese@meet.smatwebsite.com>
2024-03-28 14:51:19 +0000

    Altered settings for hosting

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "your-domain.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.0.45", "lms.smatwebsite.com"]
 
 # change the default user models to our custom model
 AUTH_USER_MODEL = "accounts.User"
--- config/wsgi.py
+++ config/wsgi.py
@@ -7,8 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+#Add virtual environment for site
+sys.path.insert(0,'/var/www/django-lms')
+sys.path.insert(0,'/var/www/django-lms/config')
+sys.path.insert(0,'/var/www/django-lms/venv/lib/python3.10/site-packages')
+print(sys.path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
