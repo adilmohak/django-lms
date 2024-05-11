c432e5770950bbbeeebedb5efb4dbc4f480206fd
papi <adilmohak123@gmail.com>
2024-05-11 22:14:59 +0300

    HTML email for new student and lecturer

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = "accounts"
+
+    def ready(self) -> None:
+        from django.db.models.signals import post_save
+        from .models import User
+        from .signals import post_save_account_receiver
+
+        post_save.connect(post_save_account_receiver, sender=User)
+
+        return super().ready()
---
+++ core/utils.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+
+def send_email(user, subject, msg):
+    send_mail(
+        subject,
+        msg,
+        settings.EMAIL_FROM_ADDRESS,
+        [user.email],
+        fail_silently=False,
+    )
+
+
+def send_html_email(subject, recipient_list, template, context):
+    """A function responsible for sending HTML email"""
+    # Render the HTML template
+    html_message = render_to_string(template, context)
+
+    # Generate plain text version of the email (optional)
+    plain_message = strip_tags(html_message)
+
+    # Send the email
+    send_mail(
+        subject,
+        plain_message,
+        settings.EMAIL_FROM_ADDRESS,
+        recipient_list,
+        html_message=html_message,
+    )
