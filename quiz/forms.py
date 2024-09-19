e035bfa35d9e3994f6ed6c7e7a09d9ae77164fc4
Sagun Devkota <sagundevyt@gmail.com>
2024-09-19 13:12:30 +0545

    fixed issue#68: Adding validation in choices.

--- quiz/forms.py
+++ quiz/forms.py
@@ -59,11 +59,41 @@ class MCQuestionForm(forms.ModelForm):
         model = MCQuestion
         exclude = ()
 
+class MCQuestionFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        """
+        Custom validation for the formset to ensure:
+        1. At least two choices are provided and not marked for deletion.
+        2. At least one of the choices is marked as correct.
+        """
+        super().clean()
+
+        # Collect non-deleted forms
+        valid_forms = [form for form in self.forms if not form.cleaned_data.get('DELETE', True)]
+
+        valid_choices = ['choice' in form.cleaned_data.keys() for form in valid_forms]
+        if(not all(valid_choices)):
+            raise forms.ValidationError("You must add a valid choice name.")
+
+        # If all forms are deleted, raise a validation error
+        if len(valid_forms) < 2:
+            raise forms.ValidationError("You must provide at least two choices.")
+
+        # Check if at least one of the valid forms is marked as correct
+        correct_choices = [form.cleaned_data.get('correct', False) for form in valid_forms]
+
+        if not any(correct_choices):
+            raise forms.ValidationError("One choice must be marked as correct.")
+        
+        if correct_choices.count(True)>1:
+            raise forms.ValidationError("Only one choice must be marked as correct.")
+
 
 MCQuestionFormSet = inlineformset_factory(
     MCQuestion,
     Choice,
     form=MCQuestionForm,
+    formset=MCQuestionFormSet,
     fields=["choice", "correct"],
     can_delete=True,
     extra=5,
