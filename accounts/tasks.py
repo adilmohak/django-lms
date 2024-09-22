33b39475f88273c008448ca7dea4d65e0be3cceb
mnymkr <hoang.tran.200032@student.fulbright.edu.vn>
2024-09-23 01:57:18 +0700

    Temporary fix to update Upskill email, will have to revert later

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -7,7 +7,7 @@ from core.utils import send_html_email
 def send_new_student_email(user_pk, password):
     user = get_user_model().objects.get(pk=user_pk)
     send_html_email(
-        subject="Your Dj LMS account confirmation and credentials",
+        subject="Your Upskill LMS account confirmation and credentials",
         recipient_list=[user.email],
         template="accounts/email/new_student_account_confirmation.html",
         context={"user": user, "password": password},
@@ -18,7 +18,7 @@ def send_new_student_email(user_pk, password):
 def send_new_lecturer_email(user_pk, password):
     user = get_user_model().objects.get(pk=user_pk)
     send_html_email(
-        subject="Your Dj LMS account confirmation and credentials",
+        subject="Your Upskill LMS account confirmation and credentials",
         recipient_list=[user.email],
         template="accounts/email/new_lecturer_account_confirmation.html",
         context={"user": user, "password": password},
